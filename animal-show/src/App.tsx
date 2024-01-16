624916e7093955c8a1a94d626756dbc7c0e2a657
JAC-SERVERS\reza.shalchian <reza.shalchian@a.com>
2024-01-16 12:50:17 -0500

    session 2

--- animal-show/src/App.tsx
+++ animal-show/src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import AnimalShow from "./components/AnimalShow";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  // const [animals, setAnimals] = useState([]);
+  // const [address, setAddress] = useState("");
+
+  const handleClick = () => {
+    // console.log("clicked");
+    // count = count + 1; NEVER think about changing the variable directly
+    setCount(count + 1);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <button
+        style={{ background: "red" }}
+        // onClick={() => console.log("clicked")}
+        onClick={handleClick}
+        // onMouseEnter={() => console.log("mouse hovered")}
+      />
+      <div>the count of animal is {count}</div>
+      <AnimalShow />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ animal-show/src/components/AnimalShow.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AnimalShow = () => {
+  return <div>AnimalShow</div>;
+};
+
+export default AnimalShow;
