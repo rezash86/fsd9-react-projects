6fe99a2f6e91d1f53d1b37a73fce2bd4d53d69e1
JAC-SERVERS\reza.shalchian <reza.shalchian@a.com>
2024-01-22 09:48:50 -0500

    adding mock

--- books-prj/src/App.tsx
+++ books-prj/src/App.tsx
@@ -4,23 +4,32 @@ import "./App.css";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 import { Book } from "./components/Book";
+import axios from "axios";
 
 function App() {
   const [books, setBooks] = useState<Book[]>([]);
 
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+    setBooks(response.data);
+  };
+
   //https://state-updates.vercel.app/#Adding%20Elements
-  const createBook = (title: string) => {
+  const createBook = async (title: string) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title,
+    });
+
     // use the state and add a new book to the existing books
-    const updatedBooks = [
-      ...books,
-      { id: Math.round(Math.random() * 9999), title },
-    ];
+    const updatedBooks = [...books, response.data];
 
     //calling setbooks is mandatory to update the state
     setBooks(updatedBooks);
   };
 
-  const deleteBookById = (id: number) => {
+  const deleteBookById = async (id: number) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
@@ -28,10 +37,14 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const editBookById = (id: number, newTitle: string) => {
+  const editBookById = async (id: number, newTitle: string) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data };
       }
       return book;
     });
@@ -48,6 +61,7 @@ function App() {
         onEdit={editBookById}
       ></BookList>
       <BookCreate onCreate={createBook}></BookCreate>
+      <button onClick={fetchBooks}>Fetch the books</button>
     </>
   );
 }
